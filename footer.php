e711504e6f6213044fffc7bb9973ca9d6f698b00
Virendra-04 <vickygodse01@gmail.com>
2025-01-08 14:16:31 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <!-- Vendor JS Files -->
-<script src="../assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="../assets/vendor/aos/aos.js"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="../assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="../assets/js/main.js"></script>
+  <script src="assets/js/main.js"></script>
 
   <footer id="footer">
 
@@ -19,7 +19,7 @@
          $year = date('Y');
        
         ?>
-        © Copyright <?= $year ?> All right reserved <strong><span>Solutions Seekers System</span></strong>
+        © Copyright <?= $year ?> All right reserved <strong><span>Solution Seekers System</span></strong>
       </div>
       
     </div>
